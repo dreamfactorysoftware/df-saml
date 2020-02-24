a7351b7c162a7320e9662a89e1d023d898b0318b
Volodymyr Polishchuk <volodymyrpo@softwareplanet.uk.com>
2020-02-24 15:01:56 +0200

    DP-172 Create an Okta connector using the df-saml package
    - Create Okta connector

---
+++ src/Models/OktaConfig.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace DreamFactory\Core\Saml\Models;
+
+use DreamFactory\Core\Components\AppRoleMapper;
+use DreamFactory\Core\Models\BaseServiceConfigModel;
+use DreamFactory\Core\Models\Role;
+
+class OktaConfig extends BaseServiceConfigModel
+{
+    use AppRoleMapper;
+
+    /** @var string */
+    protected $table = 'saml_config';
+
+    /** @var array */
+    protected $fillable = [
+        'service_id',
+        'default_role',
+        'sp_nameIDFormat',
+        'sp_x509cert',
+        'sp_privateKey',
+        'relay_state',
+        'idp_entityId',
+        'idp_singleSignOnService_url',
+        'idp_x509cert',
+        'icon_class',
+    ];
+
+    /** @var array */
+    protected $casts = [
+        'service_id'   => 'integer',
+        'default_role' => 'integer',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static function prepareConfigSchemaField(array &$schema)
+    {
+        parent::prepareConfigSchemaField($schema);
+
+        switch ($schema['name']) {
+            case 'default_role':
+                $roles = Role::whereIsActive(1)->get();
+                $roleList = [];
+
+                foreach ($roles as $role) {
+                    $roleList[] = [
+                        'label' => $role->name,
+                        'name'  => $role->id
+                    ];
+                }
+
+                $schema['type'] = 'picklist';
+                $schema['values'] = $roleList;
+                $schema['description'] = 'Select a default role for users logging in with this SAML 2.0 service type.';
+                break;
+            case 'sp_nameIDFormat':
+                $schema['label'] = 'Service Provider Name ID Format';
+                $schema['default'] = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress';
+                $schema['description'] =
+                    'Specifies the constraints on the name identifier to be used to represent the requested subject.';
+                break;
+            case 'sp_x509cert':
+                $schema['label'] = 'Service Provider X.509 Certificate';
+                $schema['description'] = 'Public x509 certificate of the Service Provider';
+                break;
+            case 'sp_privateKey':
+                $schema['label'] = 'Service Provider Private Key';
+                $schema['description'] = 'Private Key of the Service Provider';
+                break;
+            case 'relay_state':
+                $schema['label'] = 'Relay State';
+                $schema['description'] = 'The URL to redirect to upon authenticating and returning from IdP. ' .
+                    'Leaving this blank will output a JSON with authenticated user information including JWT. ' .
+                    'If you like to include the JWT in a parameter to your Relay State URL, you can do so using ' .
+                    'URL like http://example.com?jwt=_token_ . Here _token_ will be replaced by the actual JWT.';
+                break;
+            case 'idp_entityId':
+                $schema['label'] = 'Identity Provider Issuer';
+                $schema['description'] = 'Identifier of the Identity Provider entity';
+                break;
+            case 'idp_singleSignOnService_url':
+                $schema['label'] = 'Identity Provider Single Sign-On URL';
+                $schema['description'] =
+                    'URL Target of the Identity Provider where the Authentication Request Message will be sent.';
+                break;
+            case 'idp_x509cert':
+                $schema['label'] = 'Identity Provider X.509 Certificate';
+                $schema['description'] = 'Public x509 certificate of the Identity Provider.';
+                break;
+            case 'icon_class':
+                $schema['description'] = 'CSS Icon Class of the SAML 2.0 provider.';
+                break;
+        }
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace DreamFactory\Core\Saml;
 
 use DreamFactory\Core\Enums\LicenseLevel;
+use DreamFactory\Core\Saml\Models\OktaConfig;
 use DreamFactory\Core\Saml\Models\SAMLConfig;
 use DreamFactory\Core\Saml\Services\SAML;
 use DreamFactory\Core\Services\ServiceManager;
@@ -13,8 +14,23 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
+        $access_exceptions = [
+            [
+                'verb_mask' => 1,
+                'resource'  => 'sso',
+            ],
+            [
+                'verb_mask' => 2,
+                'resource'  => 'acs',
+            ],
+            [
+                'verb_mask' => 1,
+                'resource'  => 'metadata',
+            ],
+        ];
+
         // Add our service types.
-        $this->app->resolving('df.service', function (ServiceManager $df) {
+        $this->app->resolving('df.service', function (ServiceManager $df) use ($access_exceptions) {
             $df->addType(
                 new ServiceType([
                     'name'                  => 'saml',
@@ -26,20 +42,21 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                     'factory'               => function ($config) {
                         return new SAML($config);
                     },
-                    'access_exceptions'     => [
-                        [
-                            'verb_mask' => 1,
-                            'resource'  => 'sso',
-                        ],
-                        [
-                            'verb_mask' => 2,
-                            'resource'  => 'acs',
-                        ],
-                        [
-                            'verb_mask' => 1,
-                            'resource'  => 'metadata',
-                        ],
-                    ],
+                    'access_exceptions'     => $access_exceptions,
+                ])
+            );
+            $df->addType(
+                new ServiceType([
+                    'name'                  => 'okta_saml',
+                    'label'                 => 'Okta SAML',
+                    'description'           => 'Okta service supporting SSO.',
+                    'group'                 => ServiceTypeGroups::SSO,
+                    'subscription_required' => LicenseLevel::SILVER,
+                    'config_handler'        => OktaConfig::class,
+                    'factory'               => function ($config) {
+                        return new SAML($config);
+                    },
+                    'access_exceptions'     => $access_exceptions,
                 ])
             );
         });
